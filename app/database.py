d2814d7afc65013224ab33400d06ab6eefce394e
Siraphob Anuchatbut <siraphob.an@gmail.com>
2025-05-20 17:57:12 +0700

    change DB path

--- app/database.py
+++ app/database.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+from app.core.config import settings
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
