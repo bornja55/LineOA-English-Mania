bf7e7e5de243f092f682983da9797f1f8a7ce412
Preeda Anuchatbut <preeda.an@gmail.com>
2025-05-22 06:18:54 +0000

    Update code from VM Cloud

--- app/core/config.py
+++ app/core/config.py
@@ -1,18 +1,34 @@
 # app/core/config.py
+import os
 from pydantic_settings import BaseSettings
 from functools import lru_cache
+
+# Debug prints
+env_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), ".env")
+#print("DEBUG: ENV PATH =", env_path)
+#print("DEBUG: FILE EXISTS =", os.path.exists(env_path))
+#if os.path.exists(env_path):
+#    with open(env_path) as f:
+#        print("DEBUG: ENV CONTENTS\n", f.read())
+#else:
+#    print("DEBUG: .env file not found at", env_path)
 
 class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 480
     DATABASE_URL: str
-    
+
     class Config:
-        env_file = ".env"
+        env_file = env_path
 
 @lru_cache()
 def get_settings():
     return Settings()
 
 settings = get_settings()
+
+# More debug prints
+#print("DEBUG: SETTINGS SECRET_KEY =", settings.SECRET_KEY)
+#print("DEBUG: SETTINGS ALGORITHM =", settings.ALGORITHM)
+#print("DEBUG: SETTINGS DATABASE_URL =", settings.DATABASE_URL)
