895ec3b0f588557c25b12f994eda67b7e6b21c6a
Siraphob Anuchatbut <siraphob.an@gmail.com>
2025-05-29 11:25:17 +0700

    add text

--- app/models/models.py
+++ app/models/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime, Float
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime, Float, Text
 from sqlalchemy.orm import relationship
 from ..database import Base
 from datetime import datetime
