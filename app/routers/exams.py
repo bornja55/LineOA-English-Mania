4cae081774589f1303349068ba41d719e7deee09
Siraphob Anuchatbut <siraphob.an@gmail.com>
2025-06-05 13:21:10 +0700

    fix routers

--- app/routers/exams.py
+++ app/routers/exams.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from typing import List
 from sqlalchemy.orm import Session
 from ..database import get_db
-from app.auth import get_current_user
+from app.routers.auth import get_current_user
 
 router = APIRouter(
     prefix="/exams",
